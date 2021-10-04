f8169e32b7fe51615fab3f1dbdbb4003746fbee2
therealchrisccc <chriscollam@outlook.com>
2021-10-04 01:36:51 -0600

    update

---
+++ src/NumbersPrinter.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class NumbersPrinter {
+   public int[] getNumbers() {
+      Scanner scnr = new Scanner(System.in);
+      final int NUM_VALUES = 6;             
+      int[] values = new int[NUM_VALUES];  
+      int i;                                 
+
+      for (i = 0; i < values.length; ++i) {
+         values[i] = scnr.nextInt();
+      }
+      
+      return values;
+    }
+    
+   public void printSelectedNumbers() {
+      int i;
+      int[] numbers = getNumbers();
+ 
+      for (i = 0; i < numbers.length; ++i) {
+         if ((numbers[i] % 5) != 0) {
+            System.out.println(numbers[i]);   
+         }
+       }
+    }
+    
+   public static void main(String[] args) {
+      NumbersPrinter myPrinter = new NumbersPrinter();
+      myPrinter.printSelectedNumbers();
+   }
+}
