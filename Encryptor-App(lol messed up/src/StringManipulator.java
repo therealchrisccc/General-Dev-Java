d103551a5fbe38722e470ca87955fc53ee302a75
therealchrisccc <chriscollam@outlook.com>
2021-10-05 14:16:33 -0600

    fixed a minor fuck up

--- Encryptor-App(lol messed up/src/StringManipulator.java
+++ Encryptor-App(lol messed up/src/StringManipulator.java
@@ -16,24 +16,16 @@ public class StringManipulator {
 
     public static void main(String[] args){
         System.out.println(encrypt("test 123", -1));
-        System.out.println(stringCompress("You can be a good programmer if you can read this"));
-        //System.out.println(stringCompress(""));
+        System.out.println(stringCompress("compression sample"));
     }
 
     public static String manipulate(String msg, String type){
-        int shift = 0;
-        if(type.substring(0,5) == "cipher"){
-            shift = type.substring(6, (int)type.length());
-            //encrypt(msg, )
-        }
-        /*
         int i = 0;
         String hold = "";
         for(i = 0; i < msg.length(); i++){
             msg.charAt(i);
         }
         return msg;
-        */
     }
 
     public static String runLengthEncoding(String msg){
@@ -41,20 +33,7 @@ public class StringManipulator {
     }
 
     public static String stringCompress(String msg){
-        String hold = "";
-        for (String retval: msg.split(" ")){
-            if(retval.length() > 1){
-                msg = retval;
-                msg = msg.replaceAll("[" + VOWELS + "]", "");
-                hold += msg + " ";
-                //System.out.print(msg + " ");
-            }
-            else{
-                hold += retval + " ";
-                //System.out.print(retval + " ");
-            }
-        }
         msg = msg.replaceAll("[" + VOWELS + "]", "");
-        return hold;
+        return msg;
     }
 }
