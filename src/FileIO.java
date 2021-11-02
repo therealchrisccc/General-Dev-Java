71166c25cfc2d3f1897baff08fb0a393dc658476
therealchrisccc <chriscollam@outlook.com>
2021-11-02 09:10:02 -0600

    a bunch of stuff is deleted now ig

---
+++ src/FileIO.java
@@ -0,0 +1,48 @@
+/**
+ * FileIO lab for CS 1.
+ *
+ * In this lab you will be writing methods to read from a file and write output to a new file
+ *
+ * @author YOUR_NAME
+ *         YOUR_EMAIL
+ * @version 202010
+ */
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class FileIO {
+   
+   //Part 1
+    public static String[] readFile(String filename) {
+
+        //Student Code Here
+
+        //3 lines below given to students. DO NOT MODIFY
+        String[] rtn = new String[lineCounter];
+        System.arraycopy(lines, 0, rtn, 0, lineCounter);
+        return rtn;
+    }
+
+
+    //Part 2
+    public static void reverseFile(String[] parsedFile, String filename) {
+
+        //Student Code Here
+
+    }
+
+    //Part 3
+    public static void logFile(String[] parsedFile, String filename) {
+
+       //Student Code Here
+
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+
+}
